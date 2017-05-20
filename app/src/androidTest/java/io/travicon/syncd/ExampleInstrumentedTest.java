756910a00d05d40c5e3fa239a2388c2061db18c9
Tha.sheriff <danmbugua74@gmail.com>
2017-05-21 01:07:54 +0300

    Create drive connection
    - [x] Handle connection failures.
    - [x] Save file to drive.

--- app/src/androidTest/java/io/travicon/syncd/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/io/travicon/syncd/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package io.travicon.www.syncd;
+package io.travicon.syncd;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("io.travicon.www.syncd", appContext.getPackageName());
+        assertEquals("io.travicon.www.io.travicon.io.travicon.io.travicon.io.travicon.syncd", appContext.getPackageName());
     }
 }
---
+++ app/src/main/java/io/travicon/syncd/MainActivity.java
@@ -0,0 +1,148 @@
+package io.travicon.syncd;
+
+import android.content.Intent;
+import android.content.IntentSender;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi;
+import com.google.android.gms.drive.MetadataChangeSet;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+
+public class MainActivity extends AppCompatActivity implements ConnectionCallbacks,
+        OnConnectionFailedListener {
+    private GoogleApiClient mGoogleApiClient;
+    private static final String TAG = "drive-quickstart";
+    private static final int REQUEST_CODE_RESOLUTION = 3;
+    private static final int REQUEST_CODE_CREATOR = 2;
+    private static final int REQUEST_CODE_CAPTURE_IMAGE = 1;
+    private Bitmap mBitmapToSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Drive.API)
+                .addScope(Drive.SCOPE_FILE)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.i(TAG, "API client connected.");
+        if (mBitmapToSave == null) {
+            // This activity has no UI of its own. Just start the camera.
+            startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE),
+                    REQUEST_CODE_CAPTURE_IMAGE);
+            return;
+        }
+        saveFileToDrive();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "GoogleApiClient connection suspended");
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i(TAG, "GoogleApiClient connection failed: " + connectionResult.toString());
+        if (!connectionResult.hasResolution()) {
+            GoogleApiAvailability.getInstance().getErrorDialog(this, connectionResult.getErrorCode(), 0).show();
+            return;
+        }
+        // The failure has a resolution. Resolve it.
+        // Called typically when the app is not yet authorized, and an
+        // authorization
+        // dialog is displayed to the user.
+        try {
+            connectionResult.startResolutionForResult(this, REQUEST_CODE_RESOLUTION);
+        } catch (IntentSender.SendIntentException e) {
+            Log.e(TAG, "Exception while starting resolution activity", e);
+        }
+
+    }
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        switch (requestCode) {
+            case REQUEST_CODE_CREATOR:
+                Log.i(TAG, "Image successfully saved.");
+                if (resultCode == RESULT_OK) {
+                    mGoogleApiClient.connect();
+                }
+                break;
+        }
+    }
+
+    private void saveFileToDrive() {
+        // Start by creating a new contents, and setting a callback.
+        Log.i(TAG, "Creating new contents.");
+        final Bitmap image = mBitmapToSave;
+        Drive.DriveApi.newDriveContents(mGoogleApiClient).setResultCallback(new ResultCallback<DriveApi.DriveContentsResult>() {
+            @Override
+            public void onResult(DriveApi.DriveContentsResult result) {
+                // If the operation was not successful, we cannot do anything
+                // and must
+                // fail.
+                if (!result.getStatus().isSuccess()) {
+                    Log.i(TAG, "Failed to create new contents.");
+                    return;
+                }
+                // Otherwise, we can write our data to the new contents.
+                Log.i(TAG, "New contents created.");
+                // Get an output stream for the contents.
+                OutputStream outputStream = result.getDriveContents().getOutputStream();
+                // Write the bitmap data from it.
+                ByteArrayOutputStream bitmapStream = new ByteArrayOutputStream();
+                image.compress(Bitmap.CompressFormat.PNG, 100, bitmapStream);
+                try {
+                    outputStream.write(bitmapStream.toByteArray());
+                } catch (IOException e1) {
+                    Log.i(TAG, "Unable to write file contents.");
+                }
+                // Create the initial metadata - MIME type and title.
+                // Note that the user will be able to change the title later.
+                MetadataChangeSet metadataChangeSet = new MetadataChangeSet.Builder()
+                        .setMimeType("image/jpeg").setTitle("Android Photo.png").build();
+                // Create an intent for the file chooser, and start it.
+                IntentSender intentSender = Drive.DriveApi
+                        .newCreateFileActivityBuilder()
+                        .setInitialMetadata(metadataChangeSet)
+                        .setInitialDriveContents(result.getDriveContents())
+                        .build(mGoogleApiClient);
+                try {
+                    startIntentSenderForResult(intentSender, REQUEST_CODE_CREATOR, null, 0, 0, 0);
+                } catch (IntentSender.SendIntentException e) {
+                    Log.i(TAG, "Failed to launch file chooser.");
+                }
+            }
+        });
+    }
+}
